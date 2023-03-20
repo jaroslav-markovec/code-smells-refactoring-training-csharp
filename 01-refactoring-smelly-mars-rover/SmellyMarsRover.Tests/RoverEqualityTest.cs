60a15554a03f0e4a7181d32b6cfff74849ba38b8
freyes <fran@codesai.com>
2023-03-20 15:27:57 +0000

    add smelly mars rover kata

---
+++ 01-refactoring-smelly-mars-rover/SmellyMarsRover.Tests/RoverEqualityTest.cs
@@ -0,0 +1,20 @@
+using System;
+using Xunit;
+
+namespace SmellyMarsRover.Tests
+{
+    public class RoverEqualityTest
+    {
+        [Fact]
+        public void EqualRovers() {
+            Assert.Equal(new Rover(1, 1, "N"), new Rover(1, 1, "N"));
+        }
+
+        [Fact]
+        public void NotEqualRovers() {
+            Assert.NotEqual(new Rover(1, 1, "N"), new Rover(1, 1, "S"));
+            Assert.NotEqual(new Rover(1, 1, "N"), new Rover(1, 2, "N"));
+            Assert.NotEqual(new Rover(1, 1, "N"), new Rover(0, 1, "N"));
+        }
+    }
+}
---
+++ 01-refactoring-smelly-mars-rover/SmellyMarsRover/Rover.cs
@@ -0,0 +1,100 @@
+﻿using System;
+
+namespace SmellyMarsRover
+{
+    public class Rover {
+
+        private String _direction;
+        private int _y;
+        private int _x;
+
+        public Rover(int x, int y, String direction) {
+            this._direction = direction;
+            this._y = y;
+            this._x = x;
+        }
+
+        public void Receive(String commandsSequence) {
+            for (int i = 0; i < commandsSequence.Length; ++i) {
+                String command = commandsSequence.Substring(i,  1);
+
+                if (command.Equals("l") || command.Equals("r")) {
+
+                    // Rotate Rover
+                    if (_direction.Equals("N")) {
+                        if (command.Equals("r")) {
+                            _direction = "E";
+                        } else {
+                            _direction = "W";
+                        }
+                    } else if (_direction.Equals("S")) {
+                        if (command.Equals("r")) {
+                            _direction = "W";
+                        } else {
+                            _direction = "E";
+                        }
+                    } else if (_direction.Equals("W")) {
+                        if (command.Equals("r")) {
+                            _direction = "N";
+                        } else {
+                            _direction = "S";
+                        }
+                    } else {
+                        if (command.Equals("r")) {
+                            _direction = "S";
+                        } else {
+                            _direction = "N";
+                        }
+                    }
+                } else {
+
+                    // Displace Rover
+                    int displacement1 = -1;
+
+                    if (command.Equals("f")) {
+                        displacement1 = 1;
+                    }
+                    int displacement = displacement1;
+
+                    if (_direction.Equals("N")) {
+                        _y += displacement;
+                    } else if (_direction.Equals("S")) {
+                        _y -= displacement;
+                    } else if (_direction.Equals("W")) {
+                        _x -= displacement;
+                    } else {
+                        _x += displacement;
+                    }
+                }
+            }
+        }
+
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((Rover)obj);
+        }
+        
+        private bool Equals(Rover other)
+        {
+            return _direction == other._direction && _y == other._y && _x == other._x;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(_direction, _y, _x);
+        }
+
+        public override string ToString()
+        {
+            return "Rover{" +
+                "direction='" + _direction + '\'' +
+                ", y=" + _y +
+                ", x=" + _x +
+                '}';
+        }
+    }
+}
